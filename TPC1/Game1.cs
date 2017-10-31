8ea1b0501f8ab501d02204320bf900be68041110
ninjanazal <32069840+ninjanazal@users.noreply.github.com>
2017-10-31 18:58:07 +0000

    Add files via upload

---
+++ TPC1/Game1.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TPC1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        // matrix e effect geral
+        BasicEffect effect;
+        Matrix worldMatrix;
+        // Camera
+        float aspectRatio;
+
+        //piramide
+        int numSides;
+        float height;
+        clsPyramid pyramid;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            ///inicializar matrix e effect geral
+            worldMatrix = Matrix.Identity;
+            effect = new BasicEffect(GraphicsDevice);
+            aspectRatio = (float)GraphicsDevice.Viewport.Width / GraphicsDevice.Viewport.Height;
+            effect.View = Matrix.CreateLookAt(new Vector3(1.0f, 2.0f, 3.0f), new Vector3(0.0f,0.6f,0.0f), Vector3.Up);
+            effect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f), aspectRatio, 1.0f, 20.0f);
+            effect.LightingEnabled = false;
+            effect.VertexColorEnabled = true;
+
+
+            //inicializar piramide
+            numSides = 7;
+            height = 1.5f;
+
+            //numero de lados tem se ser entre 3 e 10
+            if (numSides > 10)
+                numSides = 10;
+            else if (numSides < 3)
+                numSides = 3;
+            pyramid = new clsPyramid(GraphicsDevice, effect, worldMatrix, numSides, height);
+            
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            // roda a piramide ao usar as setas direita e esquerda
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                pyramid.Rotate(0.02f);
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                pyramid.Rotate(-0.02f);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Gray);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+            spriteBatch.DrawString(Content.Load<SpriteFont>("font"), "Numero de lados: " + numSides, new Vector2(0.5f, 0.5f), Color.Black);
+            spriteBatch.End();
+
+            pyramid.Draw(GraphicsDevice);
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TPC1/clsPyramid.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TPC1
+{
+    class clsPyramid
+    {
+        Color[] sideColor;
+        VertexPositionColor[] vertex;
+        BasicEffect generalEffect;
+        Matrix generalWorldMatrix;
+        GraphicsDevice device;
+
+        Random rand;
+        float radius = 1.0f;
+        int nside;
+
+        public clsPyramid(GraphicsDevice device , BasicEffect effect, Matrix worldMatrix, int numSides, float height)
+        {
+            rand = new Random();
+            sideColor = new Color[10];
+
+            this.device = device;
+            this.generalEffect = effect;
+            this.generalWorldMatrix = worldMatrix;
+
+            this.nside = numSides;
+            
+
+            //gera aleatoriamente um array de cores
+            for (int i = 0; i < 10; i++)
+                sideColor[i] = new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));            
+
+            generatePyramid(this.nside, height,sideColor);     
+
+        }
+
+        public void generatePyramid(int nS, float h,Color[] cor)
+        {
+            vertex = new VertexPositionColor[nS * 3];
+            for(int i = 0; i<nS; i++)
+            {
+                // calcula o angulo e coordenadas dos vertices dos 2 primeiros vertices
+                float angle = i * (2 * (float)Math.PI) / nS;
+                float x = radius * (float)Math.Cos(angle);
+                float z = -radius * (float)Math.Sin(angle);
+
+                vertex[3 * i] = new VertexPositionColor(new Vector3(x, 0.0f, z), sideColor[i]);
+                vertex[3 * i + 1] = new VertexPositionColor(new Vector3(0.0f, h, 0.0f), sideColor[i]);
+
+                // calcula o angulo e coordenadas do vertice 3 de cada lado
+
+                angle = (i + 1) * (2 * (float)Math.PI) / nS;
+                x = radius * (float)Math.Cos(angle);
+                z = -radius * (float)Math.Sin(angle);
+
+                vertex[3 * i + 2] = new VertexPositionColor(new Vector3(x, 0.0f, z), sideColor[i]);
+
+            }
+        }
+
+        public void Rotate(float rotAngle)
+        {
+            generalWorldMatrix *= Matrix.CreateRotationY(rotAngle);
+        }
+
+        public void Draw(GraphicsDevice device)
+        {
+
+            generalEffect.World = generalWorldMatrix;
+            generalEffect.CurrentTechnique.Passes[0].Apply();
+            device.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.TriangleList, vertex, 0, nside);
+        }
+    }
+}
